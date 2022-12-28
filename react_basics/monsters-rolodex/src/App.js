01bba4a1c08deb3524b58324337ccfcf6bdced85
mlobf <marcoslemeborbafilho@gmail.com>
2022-12-27 23:51:53 -0300

    Component State

--- react_basics/monsters-rolodex/src/App.js
+++ react_basics/monsters-rolodex/src/App.js
@@ -3,22 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super(); //Call the underline constructor of all classes that we are extended from...
+        this.state = {
+            name: 'Marcos Leme de Oliveira Borba Filho',
+        };
+    }
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+                    <p>Hi {this.state.name}</p>
+                    <button>Change Name</button>
                 </header>
             </div>
         );
