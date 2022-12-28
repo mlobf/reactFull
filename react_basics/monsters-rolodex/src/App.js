16552e7e5e5a1b46935511d27f73daeab3afef98
mlobf <marcoslemeborbafilho@gmail.com>
2022-12-28 01:11:30 -0300

    States and Shallow Merge

--- react_basics/monsters-rolodex/src/App.js
+++ react_basics/monsters-rolodex/src/App.js
@@ -3,19 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    // Add a new obj .... the 'name' object
     constructor() {
         super(); //Call the underline constructor of all classes that we are extended from...
         this.state = {
-            name: 'Marcos Leme de Oliveira Borba Filho',
+            name: { firstName: 'Marcos', lastName: 'Filho' },
+            company: 'Kiowaki',
         };
     }
+    // Event Handler
+    //  Basic Concept => Is something that I want to happen ever a event happen
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <p>Hi {this.state.name}</p>
-                    <button>Change Name</button>
+                    <p>
+                        Hi my name is {this.state.name.firstName} and I work at
+                        {this.state.company}
+                    </p>
+                    <button
+                        onClick={() => {
+                            this.setState({
+                                name: {
+                                    firstName: 'Lin',
+                                    lastName: 'Oliveira',
+                                },
+                            });
+                        }}
+                    >
+                        Change Name
+                    </button>
                 </header>
             </div>
         );
