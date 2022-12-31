d5874dfe9c92d144494d05ec7185f00222fc9249
mlobf <marcoslemeborbafilho@gmail.com>
2022-12-31 18:53:29 -0300

    Optimazation

--- react_basics/monsters-rolodex/src/App.js
+++ react_basics/monsters-rolodex/src/App.js
@@ -2,7 +2,6 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  // Add a new obj .... the 'name' object
   constructor() {
     super(); //Call the underline constructor of all classes that we are extended from...
     this.state = {
@@ -19,22 +18,28 @@ class App extends Component {
         });
       });
   }
-  render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase(); // Bota o valor digitado em lower case
+    this.setState(() => {
+      return { searchField };
     });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
         <input
           className="search-box"
           type="search"
           placeholder="monster"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase(); // Bota o valor digitado em lower case
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+          onChange={onSearchChange}
         />
         <h1>
           {filteredMonsters.map((monster) => {
